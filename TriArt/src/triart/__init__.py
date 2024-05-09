c8cba0c209a4762a22da429aeffa74a801990103
blck-snwmn <whitesnowmancreator@gmail.com>
2024-05-10 02:43:59 +0900

    Update

--- TriArt/src/triart/__init__.py
+++ TriArt/src/triart/__init__.py
@@ -1,3 +1,9 @@
+from triart.polygonizer import polygonize_image, save_image, enhanced_polygonize_image
+
+
 def main() -> int:
-    print("Hello from triart!")
+    image_path = "16776570719onz6-300x300.jpg"  # 画像パスを適切に設定
+    result_image = enhanced_polygonize_image(image_path)
+    save_path = "path_to_save_result.jpg"  # 保存パスを適切に設定
+    save_image(result_image, save_path)
     return 0
